eea913a72a705e452cffeb0b62b01c0e8a2ebdf2
cobaltnova <benjamin.garcia@mines.sdsmt.edu>
2016-01-16 16:39:35 -0700

    edited dir structure

---
+++ GworksTests/BootstrapTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SDSMTGDT.DungeonCrawler
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class BootstrapTests
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new TestGame())
+                game.Run();
+        }
+    }
+#endif
+}
