2f93c61eed9605dd41745e8a2c85900dda115ec3
Leif Torgersen <ljtor94@yahoo.com>
2016-02-24 20:01:15 -0700

    Laid Foundation for collsion quad tree.

---
+++ SDSMT_GWorks/Physics/Collisions/ColisionTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDSMTGDT.GWorks.Physics.Collisions
+{
+    class ColisionTree
+    {
+        List<Collidable> wholeTree;
+
+    }
+}
---
+++ SDSMT_GWorks/Physics/Collisions/QuadTreeNode.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDSMTGDT.GWorks.Physics.Collisions
+{
+    interface QuadTreeNode
+    {
+        Rectangle getBounds();
+        void insert(Collidable c);
+        void delete(Collidable c);
+        List<Collidable> searchNode(Rectangle r);
+    }
+}
