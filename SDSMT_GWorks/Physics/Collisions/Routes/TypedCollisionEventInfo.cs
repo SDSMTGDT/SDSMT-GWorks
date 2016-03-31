940254432c172af56d7197f26bd70d8b6a535826
Logan Lembke <logo2462@gmail.com>
2016-03-30 18:10:28 -0600

    Missed a file

---
+++ SDSMT_GWorks/Physics/Collisions/Routes/TypedCollisionEventInfo.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDSMTGDT.GWorks.Physics.Collisions.Routes
+{
+    /// <summary>
+    /// Typed version of the CollisionEventInfo class. 
+    /// Used to convert collidables to their types in an info object.
+    /// </summary>
+    /// <typeparam name="T1">Type of the collider</typeparam>
+    /// <typeparam name="T2">Type of the collided</typeparam>
+    public class TypedCollisionEventInfo<T1, T2> where T1 : Collidable where T2 : Collidable
+    {
+        /// <summary>
+        /// The main collidable in the collision
+        /// </summary>
+        public T1 collider { get; private set; }
+
+        /// <summary>
+        /// The other collidable in the collision
+        /// </summary>
+        public T2 collided { get; private set; }
+
+        /// <summary>
+        /// Captures the rectangle of the collider at the time of the collision
+        /// </summary>
+        public Rectangle colliderBounds { get; private set; }
+
+        /// <summary>
+        /// Captures the rectangle of the collided at the time of the collision
+        /// </summary>
+        public Rectangle collidedBounds { get; private set; }
+
+        public TypedCollisionEventInfo(CollisionEventInfo info)
+        {
+            this.collider = (T1)info.collider;
+            this.collided = (T2)info.collided;
+            this.colliderBounds = new Rectangle(info.colliderBounds.Location, info.colliderBounds.Size);
+            this.collidedBounds = new Rectangle(info.collidedBounds.Location, info.collidedBounds.Size);
+        }
+
+        /// <summary>
+        /// Returns the intersection of the two bounding boxes
+        /// </summary>
+        /// <returns>The intersection of the collidable's bounding boxes</returns>
+        public Rectangle getIntersection()
+        {
+            return Rectangle.Intersect(colliderBounds, collidedBounds);
+        }
+
+        /// <summary>
+        /// Calculates the vector from the center of the collider to the center of the intersection
+        /// </summary>
+        /// <returns>A vector from the center of the collider to the center of the intersection</returns>
+        public Vector2 getCollisionVector()
+        {
+            return (getIntersection().Center - colliderBounds.Center).ToVector2();
+        }
+    }
+}
