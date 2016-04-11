f97ff8bc4e5b6951e662a902e799c30268e3338d
Wade <zalgo2462@gmail.com>
2016-04-10 22:12:50 -0600

    Fix Pong Project

--- Pong/Properties/AssemblyInfo.cs
+++ Pong/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("48109bc5-b004-4fc9-acda-5dde32e40f8f")]
+[assembly: Guid("cc9e0f82-c3bc-4121-9268-f3ec78ad2681")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.0.456")]
-[assembly: AssemblyFileVersion("3.4.0.456")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
