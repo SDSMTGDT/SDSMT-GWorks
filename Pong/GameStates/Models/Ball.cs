eb6d9ef01946b46b408849a4227ad6db950714d9
cobaltnova <benjamin.garcia@mines.sdsmt.edu>
2016-04-11 18:53:19 -0600

    Additions to the pong project.

--- Pong/GameStates/Models/Ball.cs
+++ Pong/GameStates/Models/Ball.cs
@@ -7,16 +7,25 @@ using Microsoft.Xna.Framework;
 using SDSMTGDT.GWorks.Physics;
 using SDSMTGDT.GWorks.Events;
 using Microsoft.Xna.Framework.Graphics;
+using Pong.GameStates.Drawing;
+using Pong.GameStates.Events;
+using Pong.GameStates.Updates;
 
 namespace Pong.GameStates.Models
 {
     class Ball : CollidableGameObject
     {
+        BallArtist artist;
+        BallMovement movement;
+
         Ball (Rectangle bounds, PhysicsManager physics, Texture2D texture,
             Vector2 startingVelocity, Vector2 position, GraphicsDevice graphics)
             : base(bounds, physics, texture, startingVelocity,position,graphics)
         {
-
+            artist = new BallArtist();
+            movement = new BallMovement();
         }
+
+
     }
 }
--- Pong/GameStates/Updates/BallStarter.cs
+++ Pong/GameStates/Updates/BallStarter.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using SDSMTGDT.GWorks.GameStates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Pong.GameStates.Models;
 
 namespace Pong.GameStates.Updates
 {
-    class BallStarter
+    class BallStarter : UpdateListener
     {
+        //ball which will begin moving
+        private Ball ball;
+
+        //viewport
+        private Rectangle screen;
+
+        //playstate to alter
+        private MutableGameState playState;
+
+        public void update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
