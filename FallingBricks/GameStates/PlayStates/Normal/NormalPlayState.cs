499a1ac736cc292dd53c529a375513f0793adc82
Logan Lembke <logo2462@gmail.com>
2016-04-13 20:41:34 -0600

    Start Falling Bricks.
    A lot of work left to go.

--- FallingBricks/GameStates/PlayStates/Normal/NormalPlayState.cs
+++ FallingBricks/GameStates/PlayStates/Normal/NormalPlayState.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FallingBricks.GameStates.PlayStates.Normal.Model;
+using Microsoft.Xna.Framework;
+using SDSMTGDT.GWorks.GameStates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace FallingBricks.GameStates.PlayStates.Normal
 {
-    internal class NormalPlayState
+    internal class NormalPlayState : MutableGameState
     {
+        internal int bricksWide { get; private set; }
+        internal int bricksHigh { get; private set; }
+        internal int brickSize { get; private set; }
+        private BrickBrickGraveyard graveyard {get; set;}
+
+        public NormalPlayState(GameStateManager manager) : base(manager)
+        {
+            var screenWidth = manager.settings.access(manager.settings.engineSettings.WINDOW_WIDTH);
+            var screenHeight = manager.settings.access(manager.settings.engineSettings.WINDOW_HEIGHT);
+
+            bricksWide = 10;
+            brickSize = screenWidth / bricksWide;
+            bricksHigh = screenHeight / brickSize;
+        }
     }
 }
---
+++ FallingBricks/Helpers/GraphicsObject.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallingBricks.Helpers
+{
+    interface GraphicsObject
+    {
+        Texture2D getTexture();
+
+        Rectangle getDrawBounds();
+    }
+}
