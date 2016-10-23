a73acaeaa37b30b2793be6c9a95c8f94726bbf93
cobaltnova <benjamin.garcia@mines.sdsmt.edu>
2016-10-22 19:54:09 -0600

    Added Animation and AnimationState to the GWorks extended library. TODO: Add interface for game objects.

---
+++ SDSMT_GWorks_Extended/Spriting/Animation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SDSMTGDT.GWorks.Spriting
+{
+    public class Animation
+    {
+        private Texture2D source;
+        /// <summary>
+        /// List of rectangles that define sprites in a composite image.
+        /// </summary>
+        public IReadOnlyList<Rectangle> locations { get; private set; }
+        /// <summary>
+        /// List of the bounding boxes for each sprite in the animation. Allows
+        /// for sprites that are larger or smaller than their frame bounds.
+        /// </summary>
+        public IReadOnlyList<Rectangle> bounds { get; private set; }
+        public TimeSpan frameDuration { get; private set; }
+        /// <summary>
+        /// Property that returns the number of frames in the animation based
+        /// on the list of frames in the source image.
+        /// </summary>
+        public int frameCount {
+            get
+            {
+                return locations.Count;
+            }
+            set { }
+        }
+
+        /// <summary>
+        /// Create a new animation with a set of frame locations and a source
+        /// image.
+        /// </summary>
+        /// <param name="locations">Set of rectangles that define where in the
+        /// source image the animation frame is located.</param>
+        /// <param name="source">Source image containing all of the animation
+        /// frames</param>
+        public Animation(IReadOnlyList<Rectangle> locations, IReadOnlyList<Rectangle> bounds, Texture2D source, TimeSpan frameDuration)
+        {
+            this.locations = locations;
+            this.bounds = bounds;
+            this.source = source;
+            this.frameDuration = frameDuration;
+        }
+
+        /// <summary>
+        /// Draws the animated frame to the screen.
+        /// </summary>
+        /// <param name="batch">Opened sprite batch for drawing</param>
+        /// <param name="dest">Rectangle to draw the frame in</param>
+        /// <param name="i">Index of the frame to draw</param>
+        public void draw(SpriteBatch batch, Rectangle dest, int i)
+        {
+            batch.Draw(source, dest, locations[i], Color.White);
+        }
+    }
+}
---
+++ SDSMT_GWorks_Extended/Spriting/AnimationState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SDSMTGDT.GWorks.Spriting
+{
+    public class AnimationState
+    {
+        /// <summary>
+        /// Time the animation has been running. 
+        /// Long running animations are safe.
+        /// </summary>
+        private TimeSpan accumulator;
+        private int frameNumber;
+        private Animation animation;
+        private bool looping;
+
+        AnimationState(Animation animation, bool looping)
+        {
+            this.animation = animation;
+            this.accumulator = TimeSpan.Zero;
+            this.looping = looping;
+        }
+
+        /// <summary>
+        /// Resets the animation to its initial state.
+        /// </summary>
+        public void reset()
+        {
+            this.frameNumber = 0;
+            this.accumulator = TimeSpan.Zero;
+        }
+
+        /// <summary>
+        /// Updates the animation with a given delta time. 
+        /// It may advance the current frame.
+        /// </summary>
+        /// <param name="dt">The amount of time to advance</param>
+        /// <returns>The bounding box of the current frame,
+        /// or null if the animation has finished</returns>
+        public Rectangle? update(GameTime dt)
+        {
+            accumulator += dt.ElapsedGameTime;
+            frameNumber = (int) (accumulator.Ticks / animation.frameDuration.Ticks % animation.frameCount);
+            if (!looping && accumulator.Ticks > animation.frameDuration.Ticks * animation.frameCount)
+                return null;       
+            return animation.bounds[frameNumber];
+        }
+
+        public void draw(SpriteBatch batch, Rectangle destination)
+        {
+            animation.draw(batch, destination, frameNumber);
+        }
+    }
+}
